d213a25d9ca251431306a463af6bc867e0a6a574
alireza goodarzi <alireza74goodarzi@gmail.com>
2019-09-23 11:35:42 +0330

    change docs

--- src/HamrahvasFacade.php
+++ src/HamrahvasFacade.php
@@ -5,9 +5,9 @@ namespace Alirezadp10\Hamrahvas;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static array inAppCharge(\Illuminate\Http\Request $request, int $serviceId)
- * @method static array inAppChargeConfirm(\Illuminate\Http\Request $request, int $serviceId)
- * @method static array sendSMS(string $phoneNumber, string $textMessage, int $serviceId,string $ShortCode)
+ * @method static array inAppCharge(int $serviceId, int $phoneNumber, string $chargeCodeNumber, string $price, string $description, string $content)
+ * @method static array inAppChargeConfirm(int $serviceId, int $phoneNumber, int $otpTransactionId, int $pin, string $cpUniqueToken, string $content)
+ * @method static array sendSMS(string $phoneNumber, string $textMessage, int $serviceId, string $ShortCode, int $MsgTypeCodeList, int $chargeCodeNumberList)
  */
 class HamrahvasFacade extends Facade
 {
